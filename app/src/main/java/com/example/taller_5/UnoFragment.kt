42ec73fb87ecf43e1023d895a00b0863fd345926
dagr27 <00118717@uca.edu.sv>
2019-06-14 23:35:15 -0600

    Subiendo Archivos del taller 5

---
+++ app/src/main/java/com/example/taller_5/UnoFragment.kt
@@ -0,0 +1,39 @@
+package com.example.taller_5
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.Nullable
+import androidx.navigation.Navigation
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class UnoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_uno, container, false)
+    }
+
+    override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<View>(R.id.btn_sig)
+            .setOnClickListener(Navigation.createNavigateOnClickListener(R.id.sig_fragment))
+    }
+
+
+}
